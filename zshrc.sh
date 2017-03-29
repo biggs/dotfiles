e0c52a4ba72aa9e544bdfed500a2dbf108fbda9a
FelixB <felixbig@gmail.com>
2017-03-29 16:51:24 +0100

    Tidy up .zshrc and add vicmd switch on ESC-ESC

--- zshrc.sh
+++ zshrc.sh
@@ -1,4 +1,6 @@
-############# PATH AND ADD EXTRA STUFF TO PATH #############
+####################
+# SETTING THE PATH #
+####################
 
 ## Set the path to the mac default, then add /bin as personal choice
 PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/X11/bin
@@ -17,11 +19,12 @@ export PATH="${PATH}:/usr/texbin"
 export PATH="/Users/fez/anaconda3/bin:$PATH"
 
 
-############## ZSH CUSTOMISATION ################
+###########################
+# OH-MY-ZSH RELATED STUFF #
+###########################
 
 # Path to your oh-my-zsh installation. (runs zsh customisations)
 # MOST OF THE INTERESTING STUFF IS IN /.oh-my-zsh/lib
-
 export ZSH=/Users/fez/.oh-my-zsh
 
 ZSH_THEME="mytheme"
@@ -33,10 +36,17 @@ COMPLETION_WAITING_DOTS="true"
 # PLUGINS TO LOAD
 plugins=(git z zsh-syntax-highlighting)
 
-# Add a custom manfile for z
-alias 'manz'='man /Users/fez/.oh-my-zsh/custom/plugins/z.1'
-
+# Actually import oh-my-zsh
 source $ZSH/oh-my-zsh.sh
+
+
+
+###############################
+# PERSONAL ZSH CUSTOMISATIONS #
+###############################
+
+bindkey -e  # emacs keybindings
+bindkey "^[^[" vi-cmd-mode  # temporarily switch to vi-cmd-mode (equivalent to ^X^V)
 
 # Set myself as default user
 DEFAULT_USER=fez
@@ -47,9 +57,15 @@ REPORTTIME=7
 # Stop using CTRL-S to freeze screen so can use to save in vim instead
 stty -ixon
 
+## Neovim as editor (opens really fast!)
+export VISUAL="nvim"
+export EDITOR="nvim"
 
 
-############ EDITORS ###################
+################################
+# USEFUL ALIASES AND FUNCTIONS #
+################################
+
 ## UP TO DATE EMACS AND VI
 alias emacs="emacs-25.1"
 alias vi='nvim'
@@ -57,10 +73,6 @@ alias vi='nvim'
 ## Emacs as client edit in background shortcut. (if not started, start emacs with empty alt-editor string)
 alias e="/usr/local/Cellar/emacs/25.1/bin/emacsclient --no-wait --alternate-editor=''"
 alias ec="e --create-frame"
-
-## Neovim as editor (opens really fast!)
-export VISUAL="nvim"
-export EDITOR="nvim"
 
 ## set nvim as man page reader! (N.B. automatically uses 'neoman' plugin. Personally add
 ## 'less' keybindings and Q for quit - q is remapped after start by neoman to :close)
@@ -78,10 +90,6 @@ fancy-ctrl-z () {
 }
 zle -N fancy-ctrl-z
 bindkey '^Z' fancy-ctrl-z
-
-
-
-############# MODIFY EXISTING COMMANDS + CREATE SPECIAL #############
 
 ## launch MIT scheme with a special wrapper for history and competion (rlwrap)
 alias sicp="rlwrap -r -c -f \"$HOME\"/.scheme_completion.txt mit-scheme"
