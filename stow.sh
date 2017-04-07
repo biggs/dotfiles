3b52ed990923c6e2f7b39653fe5bb36a638c264e
FelixB <felixbig@gmail.com>
2017-04-07 14:59:58 +0100

    All tangling from vim.org in blocks for 3 outputted files:
    nvim init.vim, .vimrc and vim_plugs.vim

--- stow.sh
+++ stow.sh
@@ -7,7 +7,6 @@ TANGLE_DIR="${BASEDIR}/tangled"
 
 cd "${BASEDIR}"
 stow --dir=$TANGLE_DIR --target=$HOME vim
-ln -sf $TANGLE_DIR/vim/.vimrc $TANGLE_DIR/nvim/.config/nvim/init.vim 
 stow --dir=$TANGLE_DIR --target=$HOME nvim
 stow --dir=$TANGLE_DIR --target=$HOME zsh
 
