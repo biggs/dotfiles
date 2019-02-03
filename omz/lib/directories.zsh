2e264032a30962f54516b7c928db2c1fda12c679
Felix <felixbig@gmail.com>
2019-02-03 15:14:21 -0500

    Very stripped-down version of omz included

---
+++ omz/lib/directories.zsh
@@ -0,0 +1,13 @@
+# Changing/making/removing directory
+setopt auto_pushd
+setopt pushd_ignore_dups
+setopt pushdminus
+
+alias -g ...='../..'
+alias -g ....='../../..'
+
+# List directory contents
+alias lsa='ls -lah'
+alias l='ls -lah'
+alias ll='ls -lh'
+alias la='ls -lAh'
--- zshrc.sh
+++ zshrc.sh
@@ -2,49 +2,24 @@
 # Felix Biggs zshrc #
 #####################
 
-
-########
-# PATH #
-########
-
-# SET IN .profile (so can be sourced by bash, zsh, emacs, etc.)
+# SET PATH IN .profile
 source $HOME/.profile
-
 
 
 #############################
 # Oh-My-Zsh Initializtation #
 #############################
 
-# Oh-My-Zsh is a set of Zsh plugins and customisations. It is quite heavyweight,
-# so I would like to move to something else at some point.
-
-# Most of its interesting functionality is in the oh-my-zsh/lib folder.
-
-# This sets the paths to oh-my-zsh, to a custom directory which is automatically loaded by
-# it, chooses a few settings and plugins and then actually loads it.
-
-
-# *Plugins*
-# - [[https://github.com/robbyrussell/oh-my-zsh][git]] :: shows info about an occupied git directory
-# - [[][fasd]] :: a clever script that stores recently used directories that can then be skipped to
-# - [[https://github.com/zsh-users/zsh-syntax-highlighting][zsh-syntax-highlighting]] :: installed in custom; fish like shell syntax highlighting
-
-
-export ZSH=/Users/felix/.oh-my-zsh
-export ZSH_CUSTOM=/Users/felix/Projects/dotfiles/zsh_custom
+if [[ $OSTYPE == darwin* ]]; then
+    export ZSH=~/.dotfiles/omz;
+elif [[ $OSTYPE == linux* ]]; then
+    export ZSH=/media/sf_shared/.dotfiles/omz;
+fi
 
 ENABLE_CORRECTION="true"
-COMPLETION_WAITING_DOTS="true"  # red dots
-plugins=(fasd git zsh-syntax-highlighting colored-man-pages)
-ZSH_THEME="robbyrussell"
-
-source $ZSH/oh-my-zsh.sh
-
-
-
-
-
+COMPLETION_WAITING_DOTS="true"
+plugins=(fasd git colored-man-pages)
+source $ZSH/init.sh
 
 
 
@@ -112,11 +87,6 @@ REPORTTIME=7
 # Stop using CTRL-S to freeze screen so can use to save in vim instead
 stty -ixon
 
-## Neovim as editor (opens really fast!)
-export VISUAL="nvim"
-export EDITOR="nvim"
-
-
 
 
 # Set myself as default user
@@ -137,18 +107,22 @@ stty -ixon
 # Emacs and Nvim #
 ##################
 
+## Vim/Neovim as editor (opens really fast!)
+VIM="nvim"
 
-## Neovim as editor (opens really fast!)
-export VISUAL="nvim"
-export EDITOR="nvim"
+alias vi=$VIM
+alias vim=$VIM
+alias view="$VIM -R"
+alias vdiff="$VIM -d"
 
+export VISUAL=$VIM
+export EDITOR=$VIM
+
+
+# Emacs
 alias e="/Applications/Emacs.app/Contents/MacOS/bin/emacsclient --no-wait --alternate-editor=''"
 alias ec="e --create-frame"
 
-alias vi='nvim'
-alias vim='nvim'
-alias view='nvim -R'
-alias vdiff='nvim -d'
 
 # Allow returning to Vim by pressing Ctrl+Z (actually just runs fg)
 fancy-ctrl-z () {
@@ -166,6 +140,27 @@ bindkey '^Z' fancy-ctrl-z
 
 
 
+#######################
+# MAC SPECIFIC        #
+#######################
+
+
+if [[ $OSTYPE == darwin* ]]; then
+    alias ls='exa';
+    alias l='exa -l --git';
+    alias la='exa -l -a --git';
+    alias l2='exa -l --git -T --level 2';
+
+    ## Allow using vlc
+    alias vlc='/Applications/VLC.app/Contents/MacOS/VLC'
+
+    alias led='ledger -f ~/Documents/ORG/Finances/money.ledger'
+    alias awaketime='pmset -g log | grep -e " Sleep  " -e " Wake  " | tail -n 20'
+
+    # Alias for scholar dl for google scholar articles
+    alias schdl="python3 /Users/felix/Projects/scholar_dl/scholar_dl.py"
+fi
+
 
 
 
@@ -175,22 +170,11 @@ bindkey '^Z' fancy-ctrl-z
 # Misc Useful Aliases #
 #######################
 
-
 ## launch MIT scheme with a special wrapper for history and competion (rlwrap)
 alias sicp="rlwrap -r -c -f \"$HOME\"/.scheme_completion.txt mit-scheme"
 
-## Colorize ls
-## (-F slash after directory; -G color; -h human size units with -l)
-alias ls='exa'
-alias l='exa -l --git'
-alias la='exa -l -a --git'
-alias l2='exa -l --git -T --level 2'
-
 ## Create alias gitlog, giving much more detailed output for git
 alias gitlog="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
-
-## Allow using vlc
-alias vlc='/Applications/VLC.app/Contents/MacOS/VLC'
 
 ## History search using grep
 alias hgrep='history | grep '
@@ -198,28 +182,17 @@ alias hgrep='history | grep '
 ## Download youtube audio with one command
 alias ydl="youtube-dl -x"
 
-alias led='ledger -f ~/Documents/ORG/Finances/money.ledger'
-
-alias awaketime='pmset -g log | grep -e " Sleep  " -e " Wake  " | tail -n 20'
-
-
 # Allow iTerm integration for zsh. BUT - check in iTerm first!
 [[ $TERM_PROGRAM == "iTerm.app" ]] && test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
 
-
-# Aliases for project
-alias rssh='rsync -avzhe ssh'
+# Python linting
 alias mylint='pylint --rcfile=~/.dotfiles/pylint.rc'
-
 
 # Make `help` command work like bash for builtins
 unalias run-help 2>/dev/null
 autoload run-help
 HELPDIR=/path/to/zsh_help_directory
 alias help=run-help
-
-# Alias for scholar dl for google scholar articles
-alias schdl="python3 /Users/felix/Projects/scholar_dl/scholar_dl.py"
 
 # Serve this web page
 alias httpshare='echo "WiFi IP Address: " $(ipconfig getifaddr en0) && python3 -m http.server 8000'
