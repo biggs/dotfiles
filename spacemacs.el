4a7b1a3a3ae06619741033f3519ee50c2bd90b38
Felix <felixbig@gmail.com>
2019-03-18 14:56:30 -0400

    Spacemacs to dev branch, LSP installed.

--- spacemacs.el
+++ spacemacs.el
@@ -31,39 +31,29 @@ values."
    ;; List of configuration layers to load.
    dotspacemacs-configuration-layers
    '(
-     shell-scripts
-     haskell
-     lua
-     csv
-     finance
-     python
-     octave
-     html
-     vimscript
-     yaml
-     helm
-     osx
-     auto-completion
-     ;; better-defaults
-     emacs-lisp
-     git
-     markdown
-     org
-     pandoc
-     fasd
-     latex
-     bibtex
-     (shell :variables
-            shell-default-height 30
-            shell-default-shell 'eshell
-            shell-default-term-shell "/usr/local/bin/zsh"
-            shell-default-position 'bottom)
-     spell-checking
-     ranger
-     (ipython-notebook :variables
-                       ein:jupyter-default-server-command "/usr/local/miniconda3/bin/jupyter"
-                       ein:jupyter-server-args (list "--no-browser"))
-     julia
+     ; shell-scripts
+     ; haskell
+     ; ; lsp
+     ; ; (python :variables python-backend 'lsp)
+     ; vimscript
+     ; helm
+     ; osx
+     ; auto-completion
+     ; emacs-lisp
+     ; git
+     ; markdown
+     ; org
+     ; pandoc
+     ; latex
+     ; bibtex
+     ; (shell :variables
+     ;        shell-default-height 30
+     ;        shell-default-shell 'eshell
+     ;        shell-default-term-shell "/usr/local/bin/zsh"
+     ;        shell-default-position 'bottom)
+     ; spell-checking
+     ; ranger
+     ; julia
      ;; syntax-checking
      ;; version-control
      )
@@ -74,8 +64,8 @@ values."
    dotspacemacs-additional-packages '(
                                       github-modern-theme
                                       monokai-theme
-                                      yasnippet-snippets
-                                      dash-at-point
+                                      ; yasnippet-snippets
+                                      ; dash-at-point
                                                           )
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
@@ -513,91 +503,3 @@ you should place your code here."
 )
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-;; Do not write anything past this comment. This is where Emacs will
-;; auto-generate custom variable definitions.
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["#272822" "#F92672" "#A6E22E" "#E6DB74" "#66D9EF" "#FD5FF0" "#A1EFE4" "#F8F8F2"])
- '(compilation-message-face (quote default))
- '(custom-safe-themes
-   (quote
-    ("15348febfa2266c4def59a08ef2846f6032c0797f001d7b9148f30ace0d08bcf" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "2cfc1cab46c0f5bae8017d3603ea1197be4f4fff8b9750d026d19f0b9e606fae" default)))
- '(evil-want-Y-yank-to-eol t)
- '(fci-rule-color "#3C3D37" t)
- '(highlight-changes-colors (quote ("#FD5FF0" "#AE81FF")))
- '(highlight-tail-colors
-   (quote
-    (("#3A3A3A" . 0)
-     ("#679A01" . 20)
-     ("#4BBEAE" . 30)
-     ("#1DB4D0" . 50)
-     ("#9A8F21" . 60)
-     ("#A75B00" . 70)
-     ("#F309DF" . 85)
-     ("#3A3A3A" . 100))))
- '(line-number-mode nil)
- '(magit-diff-use-overlays nil)
- '(nrepl-message-colors
-   (quote
-    ("#032f62" "#6a737d" "#d73a49" "#6a737d" "#005cc5" "#6f42c1" "#d73a49" "#6a737d")))
- '(org-babel-load-languages
-   (quote
-    ((shell . t)
-     (python . t)
-     (emacs-lisp . t)
-     (ledger . t))))
- '(package-selected-packages
-   (quote
-    (insert-shebang fish-mode company-shell julia-repl julia-mode flycheck-julia osx-dictionary transient lv intero flycheck hlint-refactor hindent helm-hoogle haskell-snippets company-ghci company-ghc ghc haskell-mode company-cabal cmm-mode dash-at-point helm-dash lua-mode org-ref pdf-tools key-chord ivy tablist helm-bibtex parsebib biblio biblio-core flatui-theme smart-mode-line-powerline-theme powerline spinner ht org-category-capture alert log4e gntp org-mime markdown-mode hydra dash-functional parent-mode projectile pkg-info epl haml-mode gitignore-mode flyspell-correct flx magit magit-popup git-commit ghub let-alist with-editor smartparens iedit anzu evil goto-chg undo-tree highlight skewer-mode request-deferred websocket request deferred js2-mode simple-httpd web-completion-data company bind-map bind-key yasnippet packed auctex anaconda-mode pythonic f dash s helm avy helm-core async auto-complete popup org-plus-contrib auctex-latexmk github-modern-theme-theme csv-mode yasnippet-snippets yapfify yaml-mode xterm-color ws-butler winum which-key web-mode volatile-highlights vimrc-mode vi-tilde-fringe uuidgen use-package toc-org tagedit spaceline smeargle slim-mode shell-pop scss-mode sass-mode reveal-in-osx-finder restart-emacs ranger rainbow-delimiters pyvenv pytest pyenv-mode py-isort pug-mode popwin pip-requirements persp-mode pcre2el pbcopy paradox pandoc-mode ox-pandoc osx-trash orgit org-projectile org-present org-pomodoro org-download org-bullets open-junk-file neotree multi-term move-text monokai-theme mmm-mode markdown-toc magit-gitflow macrostep lorem-ipsum live-py-mode linum-relative link-hint less-css-mode ledger-mode launchctl info+ indent-guide hy-mode hungry-delete htmlize hl-todo highlight-parentheses highlight-numbers highlight-indentation hide-comnt help-fns+ helm-themes helm-swoop helm-pydoc helm-projectile helm-mode-manager helm-make helm-gitignore helm-flx helm-descbinds helm-css-scss helm-company helm-c-yasnippet helm-ag google-translate golden-ratio gnuplot github-modern-theme gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link gh-md fuzzy flyspell-correct-helm flx-ido fill-column-indicator fasd fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-magit evil-lisp-state evil-indent-plus evil-iedit-state evil-exchange evil-escape evil-ediff evil-args evil-anzu eval-sexp-fu eshell-z eshell-prompt-extras esh-help emmet-mode elisp-slime-nav ein dumb-jump diminish dactyl-mode cython-mode company-web company-statistics company-auctex company-anaconda column-enforce-mode clean-aindent-mode auto-yasnippet auto-highlight-symbol auto-dictionary auto-compile aggressive-indent adaptive-wrap ace-window ace-link ace-jump-helm-line ac-ispell)))
- '(paradox-github-token t)
- '(pdf-view-midnight-colors (quote ("#6a737d" . "#fffbdd")))
- '(pos-tip-background-color "#FFFACE")
- '(pos-tip-foreground-color "#272822")
- '(vc-annotate-background nil)
- '(vc-annotate-color-map
-   (quote
-    ((20 . "#F92672")
-     (40 . "#CF4F1F")
-     (60 . "#C26C0F")
-     (80 . "#E6DB74")
-     (100 . "#AB8C00")
-     (120 . "#A18F00")
-     (140 . "#989200")
-     (160 . "#8E9500")
-     (180 . "#A6E22E")
-     (200 . "#729A1E")
-     (220 . "#609C3C")
-     (240 . "#4E9D5B")
-     (260 . "#3C9F79")
-     (280 . "#A1EFE4")
-     (300 . "#299BA6")
-     (320 . "#2896B5")
-     (340 . "#2790C3")
-     (360 . "#66D9EF"))))
- '(vc-annotate-very-old-color nil)
- '(weechat-color-list
-   (unspecified "#272822" "#3A3A3A" "#F70057" "#F92672" "#86C30D" "#A6E22E" "#BEB244" "#E6DB74" "#40CAE4" "#66D9EF" "#FB35EA" "#FD5FF0" "#74DBCD" "#A1EFE4" "#F8F8F2" "#F8F8F0")))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Source Code Pro" :foundry "nil" :slant normal :weight normal :height 151 :width normal)))))
