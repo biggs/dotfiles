17d49b3e8738bd04c9c2b99e27ea017fa502077f
Felix <felixbig@gmail.com>
2019-11-01 22:51:48 +0000

    Remove things from config repeated in DOOM.

--- emacs/init.el
+++ emacs/init.el
@@ -173,4 +173,4 @@
        ;; The default module sets reasonable defaults for Emacs. It also
        ;; provides a Spacemacs-inspired keybinding scheme and a smartparens
        ;; config. Use it as a reference for your own modules.
-       (default +bindings))
+       (default +bindings +smartparens))
