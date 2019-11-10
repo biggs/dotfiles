718d60f62f871f2512110ca6692fad2cb162768f
Felix <felixbig@gmail.com>
2019-11-10 22:19:25 +0000

    Switch from HELM to IVY (better Doom integration).

--- emacs/init.el
+++ emacs/init.el
@@ -11,9 +11,9 @@
 (doom! :input
        :completion
        company           ; the ultimate code completion backend
-       helm              ; the *other* search engine for love and life
+       ;; helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       ;; ivy               ; a search engine for love and life
+       ivy               ; a search engine for love and life
 
        :ui
        ;;deft              ; notational velocity for Emacs
