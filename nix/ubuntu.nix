b977fec3f4e4317ad97d741e34bf147f8eb30179
Felix <felixbig@gmail.com>
2019-12-01 15:51:53 +0000

    Switch Ubuntu to home-manager dotfiles.

--- nix/ubuntu.nix
+++ nix/ubuntu.nix
@@ -2,6 +2,11 @@
 
 with import <nixpkgs> {};
 
+let
+  # Personal Info
+  name = "Felix Biggs";
+  email = "felixbig@gmail.com";
+in
 {
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
@@ -16,6 +21,25 @@ with import <nixpkgs> {};
       pkgs.xcape
       watchexec
     ];
+  # Link dotfiles into place.
+  home.file = {
+    # Re-tangle doom config on rebuild (breaks goto doom-private-dir).
+    ".config/doom" = {
+      source = ../emacs;
+      recursive = true;
+      onChange = ''~/.emacs.d/bin/doom refresh'';
+    };
+
+    ".config/fish/config.fish".source = ../fish/config.fish;
+    ".gitconfig".source = ../git/gitconfig;
+    ".config/nvim/init.vim".source = ../vim/init.vim;
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "${name}";
+    userEmail = "${email}";
+  };
 
 
 }
