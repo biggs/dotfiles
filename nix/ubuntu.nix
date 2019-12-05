8001ef2fa8f262897b375f2a914ced79819518c7
Felix Biggs <felixbig@gmail.com>
2019-12-05 13:57:49 +0000

    Add Ubuntu .profile with locale fix.

--- nix/ubuntu.nix
+++ nix/ubuntu.nix
@@ -15,5 +15,11 @@
     anki
     qtpass
     spotify
+    glibcLocales   # Fix locales (along with setting in .profile: https://github.com/NixOS/nix/issues/599#issuecomment-131576553)
+    feh
   ];
+
+  home.file = {
+    ".profile".source = ../ubuntu/profile-ubuntu;
+  };
 }
